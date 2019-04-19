478efe01396a908e71fc037c3fab24cfe246f0c2
Denley Hsiao <denley@justtodo.com>
2019-04-19 12:30:52 +0800

    Add config for run.sh.

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,11 @@ set -o errexit
 WORK_PATH=$(cd $(dirname $0); pwd)
 BUILD_PATH=$WORK_PATH/../build
 
-$BUILD_PATH/demo/demo
+if [ -z $PROJECT_NAME ]; then
+  PROJECT_NAME=demo
+fi
+if [ -z $PROGRAM_NAME ]; then
+  PROGRAM_NAME=$PROJECT_NAME
+fi
+
+$BUILD_PATH/$PROJECT_NAME/$PROGRAM_NAME
