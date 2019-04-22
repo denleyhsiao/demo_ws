74841148d5a9e41c1793c8a20ad96af7a0752e24
Denley Hsiao <denley@justtodo.com>
2019-04-22 11:40:40 +0800

    Support config param.

--- scripts/build_run.sh
+++ scripts/build_run.sh
@@ -3,5 +3,13 @@
 set -o errexit
 
 WORK_PATH=$(cd $(dirname $0); pwd)
+
+if [ -z $PROJECT_NAME ]; then
+  PROJECT_NAME=demo
+fi
+if [ -z $PROGRAM_NAME ]; then
+  PROGRAM_NAME=$PROJECT_NAME
+fi
+
 $WORK_PATH/build.sh
-$WORK_PATH/run.sh
+PROJECT_NAME=$PROJECT_NAME PROGRAM_NAME=$PROGRAM_NAME $WORK_PATH/run.sh
